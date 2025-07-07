68d5eb35b665d499d2ee493482fa367ccf467501
Benoit Orihuela <bobeal@pm.me>
2025-07-07 01:21:41 +0100

    chore: update license headers (#19)

--- jolt-core/src/test/java/io/joltcommunity/jolt/SortrTest.java
+++ jolt-core/src/test/java/io/joltcommunity/jolt/SortrTest.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2013 Bazaarvoice, Inc.
+ * Copyright 2013-2023 Bazaarvoice, Inc.
+ * Copyright 2025 Jolt Community
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
