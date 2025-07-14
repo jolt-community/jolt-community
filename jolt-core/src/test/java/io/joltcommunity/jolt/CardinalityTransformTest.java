d316f63fca848a8ddf7f2dc09cb0109745074f32
Sun Yimin <emmansun@users.noreply.github.com>
2025-07-14 17:23:45 +0800

    chore(cardinality): add null scalar input data case

--- jolt-core/src/test/java/io/joltcommunity/jolt/CardinalityTransformTest.java
+++ jolt-core/src/test/java/io/joltcommunity/jolt/CardinalityTransformTest.java
@@ -58,6 +58,7 @@ public class CardinalityTransformTest {
                 {"starRegexTestData"},
                 {"thisLevelIsNull"},
                 {"scalarInputData"},
+                {"nullScalarInputData"},
                 {"atTestData"}
         };
     }
