f4f0d0d49491f2e90760018c1058cefc60294d28
Benoit Orihuela <benoit.orihuela@egm.io>
2026-02-21 17:09:02 +0100

    fix: still more edge cases

--- jolt-core/src/test/java/io/joltcommunity/jolt/modifier/function/DatesTest.java
+++ jolt-core/src/test/java/io/joltcommunity/jolt/modifier/function/DatesTest.java
@@ -31,11 +31,15 @@ public class DatesTest extends AbstractTester {
     @DataProvider(parallel = false)
     public Iterator<Object[]> getTestCases() {
         List<Object[]> testCases = new LinkedList<>();
+        Function NOW = new Dates.now();
         Function TO_EPOCH = new Dates.toEpochMilli();
         Function FROM_EPOCH = new Dates.fromEpochMilli();
         Function FORMAT = new Dates.format();
         Function DATE_ADD = new Dates.dateAdd();
         Function DATE_SUBSTRACT = new Dates.dateSubstract();
+
+        testCases.add(new Object[]{"now-pattern-invalid", NOW, new Object[]{"ABCD", "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"now-pattern-int", NOW, new Object[]{1, "UTC"}, Optional.empty()});
 
         testCases.add(new Object[]{"fromEpoch-default-long", FROM_EPOCH, new Object[]{1L}, Optional.of("1970-01-01T00:00:00Z")});
         testCases.add(new Object[]{"fromEpoch-pattern-long", FROM_EPOCH, new Object[]{1L, "yyyy", "UTC"}, Optional.of("1970")});
@@ -45,27 +49,33 @@ public class DatesTest extends AbstractTester {
         testCases.add(new Object[]{"fromEpoch-pattern-iso8601", FROM_EPOCH, new Object[]{1771176362001L, "yyyy-MM-dd'T'HH:mm:ssX", "Europe/Paris"}, Optional.of("2026-02-15T18:26:02+01")});
 
         testCases.add(new Object[]{"fromEpoch-pattern-null", FROM_EPOCH, null, Optional.empty()});
-        testCases.add(new Object[]{"fromEpoch-pattern-numeric-pattern", FROM_EPOCH, new Object[]{1, 1}, Optional.empty()});
-        testCases.add(new Object[]{"fromEpoch-pattern-invalid-pattern", FROM_EPOCH, new Object[]{1, "ABCD"}, Optional.empty()});
-        testCases.add(new Object[]{"fromEpoch-default-string", FROM_EPOCH, new Object[]{"1"}, Optional.empty()});
-        testCases.add(new Object[]{"fromEpoch-pattern-string", FROM_EPOCH, new Object[]{"1", "yyyy"}, Optional.empty()});
-        testCases.add(new Object[]{"fromEpoch-null", FROM_EPOCH, new Object[]{null}, Optional.empty()});
+        testCases.add(new Object[]{"fromEpoch-pattern-numeric", FROM_EPOCH, new Object[]{1, 1, "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"fromEpoch-pattern-invalid", FROM_EPOCH, new Object[]{1, "ABCD", "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"fromEpoch-epoch-string", FROM_EPOCH, new Object[]{"1", "yyyy", "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"fromEpoch-null-args", FROM_EPOCH, new Object[]{null}, Optional.empty()});
 
         testCases.add(new Object[]{"toEpoch-pattern-day", TO_EPOCH, new Object[]{"2000-01-01", "yyyy-MM-dd", "UTC"}, Optional.of(946684800000L)});
         testCases.add(new Object[]{"toEpoch-pattern-seconds", TO_EPOCH, new Object[]{"2000-01-01T00:00:00Z", "yyyy-MM-dd'T'HH:mm:ss'Z'", "UTC"}, Optional.of(946684800000L)});
         testCases.add(new Object[]{"toEpoch-pattern-seconds", TO_EPOCH, new Object[]{"2000-01-01T00:00:00Z", "yyyy-MM-dd'T'HH:mm:ss'Z'", "Europe/Paris"}, Optional.of(946681200000L)});
         testCases.add(new Object[]{"toEpoch-pattern-milliseconds", TO_EPOCH, new Object[]{"2000-01-01T00:00:00.000Z", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "UTC"}, Optional.of(946684800000L)});
-        testCases.add(new Object[]{"toEpoch-pattern-no-pattern", TO_EPOCH, new Object[]{"1970-01-01"}, Optional.empty()});
-        testCases.add(new Object[]{"toEpoch-pattern-invalid-data", TO_EPOCH, new Object[]{1}, Optional.empty()});
-        testCases.add(new Object[]{"toEpoch-pattern-null", TO_EPOCH, null, Optional.empty()});
+
+        testCases.add(new Object[]{"toEpoch-pattern-missing", TO_EPOCH, new Object[]{"1970-01-01"}, Optional.empty()});
+        testCases.add(new Object[]{"toEpoch-timezone-missing", TO_EPOCH, new Object[]{"1970-01-01", "yyyy-MM-dd"}, Optional.empty()});
+        testCases.add(new Object[]{"toEpoch-pattern-invalid", TO_EPOCH, new Object[]{"1970-01-01", "ABCD", "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"toEpoch-pattern-numeric", TO_EPOCH, new Object[]{"1970-01-01", 1, "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"toEpoch-date-invalid", TO_EPOCH, new Object[]{1}, Optional.empty()});
+        testCases.add(new Object[]{"toEpoch-null-args", TO_EPOCH, null, Optional.empty()});
 
         testCases.add(new Object[]{"format-date-only", FORMAT, new Object[]{"2000-01-01", "yyyy-MM-dd", "yyyyMMdd", "UTC"}, Optional.of("20000101")});
         testCases.add(new Object[]{"format-with-time", FORMAT, new Object[]{"2000-01-01T12:30:45", "yyyy-MM-dd'T'HH:mm:ss", "yyyy-MM-dd HH:mm:ss", "UTC"}, Optional.of("2000-01-01 12:30:45")});
         testCases.add(new Object[]{"format-with-time", FORMAT, new Object[]{"2000-01-01T12:30:45", "yyyy-MM-dd'T'HH:mm:ss", "yyyy-MM-dd HH:mm:ss", "Europe/Paris"}, Optional.of("2000-01-01 12:30:45")});
         testCases.add(new Object[]{"format-to-iso8601", FORMAT, new Object[]{"20000101", "yyyyMMdd", "yyyy-MM-dd'T'HH:mm:ss'Z'", "UTC"}, Optional.of("2000-01-01T00:00:00Z")});
-        testCases.add(new Object[]{"format-invalid-args", FORMAT, new Object[]{"2000-01-01", "yyyy-MM-dd"}, Optional.empty()});
-        testCases.add(new Object[]{"format-invalid-pattern", FORMAT, new Object[]{"2000-01-01", "INVALID", "yyyy-MM-dd"}, Optional.empty()});
-        testCases.add(new Object[]{"format-null", FORMAT, null, Optional.empty()});
+
+        testCases.add(new Object[]{"format-timezone-missing", FORMAT, new Object[]{"2000-01-01", "yyyy-MM-dd"}, Optional.empty()});
+        testCases.add(new Object[]{"format-from-pattern-invalid", FORMAT, new Object[]{"2000-01-01", "ABCD", "yyyy-MM-dd", "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"format-to-pattern-invalid", FORMAT, new Object[]{"2000-01-01", "yyyy-MM-dd", "ABCD", "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"format-pattern-numeric", FORMAT, new Object[]{"2000-01-01", "yyyy-MM-dd", 1, "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"format-null-args", FORMAT, null, Optional.empty()});
 
         testCases.add(new Object[]{"dateAdd-one-day", DATE_ADD, new Object[]{"2000-01-01", "yyyy-MM-dd", "P1D", "UTC"}, Optional.of("2000-01-02")});
         testCases.add(new Object[]{"dateAdd-one-month", DATE_ADD, new Object[]{"2000-01-01", "yyyy-MM-dd", "P1M", "UTC"}, Optional.of("2000-02-01")});
@@ -74,9 +84,11 @@ public class DatesTest extends AbstractTester {
         testCases.add(new Object[]{"dateAdd-with-time", DATE_ADD, new Object[]{"2000-01-01T12:30:45", "yyyy-MM-dd'T'HH:mm:ss", "P1D", "UTC"}, Optional.of("2000-01-02T12:30:45")});
         testCases.add(new Object[]{"dateAdd-with-time", DATE_ADD, new Object[]{"2000-01-01T12:30:45", "yyyy-MM-dd'T'HH:mm:ss", "P1D", "Europe/Paris"}, Optional.of("2000-01-02T12:30:45")});
         testCases.add(new Object[]{"dateAdd-with-time-one-hour", DATE_ADD, new Object[]{"2000-01-01T12:30:45", "yyyy-MM-dd'T'HH:mm:ss", "PT1H", "UTC"}, Optional.of("2000-01-01T13:30:45")});
-        testCases.add(new Object[]{"dateAdd-invalid-args", DATE_ADD, new Object[]{"2000-01-01", "yyyy-MM-dd"}, Optional.empty()});
-        testCases.add(new Object[]{"dateAdd-invalid-duration", DATE_ADD, new Object[]{"2000-01-01", "yyyy-MM-dd", "INVALID"}, Optional.empty()});
-        testCases.add(new Object[]{"dateAdd-null", DATE_ADD, null, Optional.empty()});
+
+        testCases.add(new Object[]{"dateAdd-timezone-missing", DATE_ADD, new Object[]{"2000-01-01", "yyyy-MM-dd", "P1D"}, Optional.empty()});
+        testCases.add(new Object[]{"dateAdd-duration-invalid", DATE_ADD, new Object[]{"2000-01-01", "yyyy-MM-dd", "INVALID", "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"dateAdd-duration-numeric", DATE_ADD, new Object[]{"2000-01-01", "yyyy-MM-dd", 1, "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"dateAdd-null-args", DATE_ADD, null, Optional.empty()});
 
         testCases.add(new Object[]{"dateSubstract-one-day", DATE_SUBSTRACT, new Object[]{"2000-01-02", "yyyy-MM-dd", "P1D", "UTC"}, Optional.of("2000-01-01")});
         testCases.add(new Object[]{"dateSubstract-one-month", DATE_SUBSTRACT, new Object[]{"2000-02-01", "yyyy-MM-dd", "P1M", "UTC"}, Optional.of("2000-01-01")});
@@ -84,9 +96,11 @@ public class DatesTest extends AbstractTester {
         testCases.add(new Object[]{"dateSubstract-complex", DATE_SUBSTRACT, new Object[]{"2001-03-04", "yyyy-MM-dd", "P1Y2M3D", "UTC"}, Optional.of("2000-01-01")});
         testCases.add(new Object[]{"dateSubstract-with-time", DATE_SUBSTRACT, new Object[]{"2000-01-02T12:30:45", "yyyy-MM-dd'T'HH:mm:ss", "P1D", "UTC"}, Optional.of("2000-01-01T12:30:45")});
         testCases.add(new Object[]{"dateSubstract-with-time-one-hour", DATE_SUBSTRACT, new Object[]{"2000-01-01T12:30:45", "yyyy-MM-dd'T'HH:mm:ss", "PT1H", "UTC"}, Optional.of("2000-01-01T11:30:45")});
-        testCases.add(new Object[]{"dateSubstract-invalid-args", DATE_SUBSTRACT, new Object[]{"2000-01-01", "yyyy-MM-dd"}, Optional.empty()});
-        testCases.add(new Object[]{"dateSubstract-invalid-duration", DATE_SUBSTRACT, new Object[]{"2000-01-01", "yyyy-MM-dd", "INVALID"}, Optional.empty()});
-        testCases.add(new Object[]{"dateSubstract-null", DATE_SUBSTRACT, null, Optional.empty()});
+
+        testCases.add(new Object[]{"dateSubstract-timezone-missing", DATE_SUBSTRACT, new Object[]{"2000-01-01", "yyyy-MM-dd", "P1D"}, Optional.empty()});
+        testCases.add(new Object[]{"dateAdd-duration-invalid", DATE_SUBSTRACT, new Object[]{"2000-01-01", "yyyy-MM-dd", "INVALID", "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"dateAdd-duration-numeric", DATE_SUBSTRACT, new Object[]{"2000-01-01", "yyyy-MM-dd", 1, "UTC"}, Optional.empty()});
+        testCases.add(new Object[]{"dateSubstract-null-args", DATE_SUBSTRACT, null, Optional.empty()});
 
         return testCases.iterator();
     }
